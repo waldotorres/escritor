26e86c7037e4ffdf52d13bd3df7482aaeca9305c
Waldo Torres <waldotorres_82@hotmail.com>
2023-09-05 23:14:28 -0500

    first commit

---
+++ escritor_net452/Escritor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Escritor
+{
+    public static  SqlString Escribir(string nombre, string directorio, string texto)
+    {
+        try
+        {
+            string fullName = directorio + @"\" + nombre;
+            using (StreamWriter sw = new StreamWriter(fullName))
+            {
+                sw.Write(texto);
+                return new SqlString("ok");
+            }
+            //return new SqlString( "ok");
+        }
+        catch (Exception ex)
+        {
+
+            return new SqlString( ex.Message);
+        }
+
+    }
+}
